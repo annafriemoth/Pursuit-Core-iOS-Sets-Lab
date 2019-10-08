0182b54df228849aa302ff2e97fe2cbf255a9715
Anna Friemoth <annafriemoth@gmail.com>
2019-10-08 16:46:58 -0400

    updated

--- Pursuit-Core-iOS-Sets-Lab.playground/Contents.swift
+++ Pursuit-Core-iOS-Sets-Lab.playground/Contents.swift
@@ -6,23 +6,41 @@ import UIKit
 
 // Create a new array numbersWithNoDuplicates that has all of the elements from numbers without any duplicates.  It should be in the same order as the original.
 
-let numbers = [1,1,2,4,4,4,6,6,7,8]
 
-var numbersWithNoDuplicates = [Int]()
+
 
 // Your code here
 
+let numbers: Set<Int> = [1,1,2,4,4,4,6,6,7,8]
+let numbersWithNoDuplicates = numbers.sorted()
+print(numbersWithNoDuplicates)
+
 //assert(numbersWithNoDuplicates == [1,2,4,6,7,8], "Was expecting [1,2,4,6,7,8], but got \(numbersWithNoDuplicates)")
+
+
+
 
 // Questions Two
 
 // Create a new array scoresThatAppearOnce that has all the elements from scores that appear exactly once.  It should be in the same order as the original.
 
-let scores = [1, 77, 83, 32, 77, 77, 83, 32, 99]
-
-var scoresThatAppearOnce = [Int]()
-
 // Your code here
+
+let scores = [1, 77, 83, 32, 77, 77, 83, 32, 99]
+var scoresThatAppearOnce = [Int]()
+var scoreCounts = [Int:Int]()
+ var x = 0
+ for score in scores.sorted() {
+     scoreCounts[score] = (scoreCounts[score] ?? 0) + 1
+ }
+ for (key,value) in scoreCounts {
+     if value == 1 {
+         scoresThatAppearOnce.append(key)
+     }
+ }
+ scoresThatAppearOnce = scoresThatAppearOnce.sorted()
+ assert(scoresThatAppearOnce == [1, 99], "Was expecting [1, 99], but got \(scoresThatAppearOnce)")
+    
 
 //assert(scoresThatAppearOnce == [1, 99], "Was expecting [1, 99], but got \(scoresThatAppearOnce)")
 
@@ -32,12 +50,14 @@ var scoresThatAppearOnce = [Int]()
 
 // Given arrOne and arrTwo, create a variable arrThree which is equal to the UNION of arrOne and arrTwo.  It should not contain any duplicate elements.  Sort arrThree from smallest to greatest.
 
-let arrOne = [1,2,3,4,5]
-let arrTwo = [3,4,5,6,7]
-
-var arrThree: [Int] = []
-
 // Your code here
+
+let arrOne: Set<Int> = [1,2,3,4,5]
+let arrTwo: Set<Int> = [3,4,5,6,7]
+
+var arrThree: Set<Int> = []
+arrThree = arrOne.union(arrTwo)
+print(arrThree.sorted())
 
 //assert(arrThree == [1,2,3,4,5,6,7], "Was expecting [1,2,3,4,5,6,7], but got \(arrThree)")
 
@@ -45,12 +65,15 @@ var arrThree: [Int] = []
 
 // Given arrFour and arrFive, create a variable arrSix with is equal to the INTERSECTION of arrFour and arrFive.  If should not contain any duplicate elements.  Sort arrSix from smallest to greatest.
 
-let arrFour = [1,2,3,4,5]
-let arrFive = [3,4,5,6,7]
-
-var arrSix: [Int] = []
-
 // Your code here
+
+
+let arrFour: Set<Int> = [1,2,3,4,5]
+let arrFive: Set<Int> = [3,4,5,6,7]
+var arrSix: Set<Int> = []
+arrSix = arrFour.intersection(arrFive)
+print(arrSix.sorted())
+
 
 //assert(arrSix == [3,4,5], "Was expecting [3,4,5], but got \(arrSix)")
 
@@ -58,12 +81,12 @@ var arrSix: [Int] = []
 
 // Given the 4 arrays of Ints below, create a new array, sorted in ascending order, that contains all the values without duplicates.
 
-let numsOne = [2, 4, 5, 6, 8, 10, 12]
-let numsTwo = [1, 2, 3, 4, 5, 6]
-let numsThree = [5, 6, 7, 8, 9, 10, 11, 12]
-let numsFour = [1, 3, 4, 5, 6, 7, 9]
-
-var allNumsWithNoDuplicates: [Int] = []
+let numsOne: Set<Int>  = [2, 4, 5, 6, 8, 10, 12]
+let numsTwo: Set<Int>  = [1, 2, 3, 4, 5, 6]
+let numsThree: Set<Int>  = [5, 6, 7, 8, 9, 10, 11, 12]
+let numsFour: Set<Int>  = [1, 3, 4, 5, 6, 7, 9]
+var allNumsWithNoDuplicates: Set<Int> = []
+allNumsWithNoDuplicates = 
 
 // Your code here
 
